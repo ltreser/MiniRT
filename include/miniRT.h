ac17ab2d4ca1c2b97c79818b4afc5bc83d2b7719
afoth <afoth@student.42berlin.de>
2024-11-19 19:49:56 +0100

    added header

--- include/miniRT.h
+++ include/miniRT.h
@@ -1,19 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   miniRT.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: afoth <afoth@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/19 16:23:05 by ltreser           #+#    #+#             */
-/*   Updated: 2024/11/19 16:59:18 by afoth            ###   ########.fr       */
+/*   Created: 2024/11/19 18:04:10 by afoth             #+#    #+#             */
+/*   Updated: 2024/11/19 18:04:24 by afoth            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "minirt.h"
-#include <stdio.h>
+#ifndef MINI_RT_H
+# define MINI_RT_H
 
-int	main	(void)
-{
-	printf("HELLO");
-}
+/* Standard Libraries */
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <math.h>
+
+/* MiniLibX */
+# include <mlx.h>
+
+
+
+#endif /* MINI_RT_H */
