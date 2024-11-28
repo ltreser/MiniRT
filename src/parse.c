50eb57e9598a3bcef177534b99f555cf3aa8e33e
Lisamarie Treser <ltreser@c4a3c8.42berlin.de>
2024-11-28 22:28:26 +0100

    feat: malloc and parse obj ft

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: afoth <afoth@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 17:11:57 by ltreser           #+#    #+#             */
-/*   Updated: 2024/11/28 21:23:58 by ltreser          ###   ########.fr       */
+/*   Updated: 2024/11/28 22:27:57 by ltreser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,20 @@ void    ft_parse(char *str, t_rt *rt)
 
 void	parse_obj(char *str, t_rt *rt, char type)
 {
-	//TODO malloc for obj
-	parse_point(ft_chop(str), rt);
+	rt->obj[n_obj] = malloc(sizeof(t_obj));
+	rt->obj[n_obj]->type = (t_enum)type;
+	if (rt->obj[n_obj]->type == SPHERE)
+		rt->obj[n_obj]->sphere = malloc(sizeof(t_sphere));
+	if (rt->obj[n_obj]->type == PLANE)
+		rt->obj[n_obj]->plane = malloc(sizeof(t_plane));
+	if (rt->obj[n_obj]->type == CYLINDER)
+		rt->obj[n_obj]->cylinder = malloc(sizeof(t_cylinder));
+	rt->obj[n_obj]->sphere->parse_point(ft_chop(str, ' '), rt);
 	parse_vector(ft_chop(str), rt);
-	if (type == 'c')
+	if (type == CYLINDER)
 		parse_dimensions(str, rt);
 	parse_color(ft_chop(str), rt);
-	rt->obj->n++;
+	rt->n_obj++;
 }
 
 void	parse_point(t_rt *rt, t_point point, char *str)
