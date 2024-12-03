ee39c3467c2f5a03ef6c26748ba1c8077a7dabe4
Lisamarie Treser <lisamarietreser@yahoo.de>
2024-12-03 18:12:31 +0100

    chore: add mlx struct

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: afoth <afoth@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 16:23:05 by ltreser           #+#    #+#             */
-/*   Updated: 2024/12/03 17:36:17 by afoth            ###   ########.fr       */
+/*   Updated: 2024/12/03 18:09:47 by ltreser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,11 @@ int	main(int argc, char **argv)
 {
 	t_rt *rt;
 	int fd;
-	struct timeval	start, end;
 
-	gettimeofday(&start, NULL);
 	rt = malloc(sizeof(rt));
 	fd = 0;
-	fd = check_input(rt, argc, argv);
+	fd = check_input;
 	read_file(fd, rt);
-		//render etc
-	gettimeofday(&end, NULL);
-	double runtime = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1e6;
-	free(rt);
-	printf("Runtime: %.6f seconds\n", runtime);
+	mlx_create_window(rt);
+	//render(rt);
 }
