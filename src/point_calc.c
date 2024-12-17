7026c096c6ba034ee9117aefe02a6f7bb72f7cd5
afoth <afoth@student.42berlin.de>
2024-12-17 17:49:27 +0100

    worked on masks for obj to optimize rendering, merged

--- src/point_calc.c
+++ src/point_calc.c
@@ -1,38 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   math_utils.c                                       :+:      :+:    :+:   */
+/*   point_calc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: afoth <afoth@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/17 15:53:26 by afoth             #+#    #+#             */
-/*   Updated: 2024/12/17 15:53:27 by afoth            ###   ########.fr       */
+/*   Created: 2024/12/17 17:45:50 by afoth             #+#    #+#             */
+/*   Updated: 2024/12/17 17:46:36 by afoth            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/miniRT.h"
 
-t_vec	*v_cross_product(t_vec *a, t_vec *b)
+t_point   *pv_add(t_vec *a, t_point *b)
 {
-	t_vec *res;
+    t_point *res;
 
-	res = malloc(sizeof(t_vec));
-	res->x = a->y * b->z - a->z * b->y;
-	res->y = a->z * b->x - a->x * b->z;
-	res-> z = a->x * b->y - a->y * b->x;
-	return (res);
+    res = gc_malloc(rt->gc, sizeof(t_point));
+    res->x = a->x + b->x;
+    res->y = a->y + b->y;
+    res->z = a->z + b->z;
+    return (res);
 }
-
-t_vec	*v_normalize(t_vec *v)
+t_point	calc_endpoint_vector(t_vector *v, t_point *start, float scalar)
 {
-	float	length;
+	t_point	 end;
 
-	length = 0;
-	length = sqrtf(v->x * v->x + v->y * v->y + v->z * v->z);
-	if (!length)
-		return ((t_vec{0,0,0}));
-	v->x /= length;
-	v->y /= length;
-	v->z /= length;
-	return (v);
+	end.x = start->x + scalar * v->x;
+	end.y = start->y + scalar * v->y;
+	end.z = start->z + scalar * v->z;
+	return(end);
 }
--- src/vector_calc.c
+++ src/vector_calc.c
@@ -6,7 +6,7 @@
 /*   By: afoth <afoth@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 23:10:32 by afoth             #+#    #+#             */
-/*   Updated: 2024/12/17 17:39:10 by afoth            ###   ########.fr       */
+/*   Updated: 2024/12/17 17:49:22 by afoth            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,58 +17,38 @@ float v_len(t_vector *vector)
 	return(sqrtf(vector->x * vector->x + vector->y * vector->y + vector->z * vector->z));
 }
 
-t_vec   *v_cross_product(t_vec *a, t_vec *b)
+t_vector	*v_cross_product(t_rt *rt, t_vector *a, t_vector *b)
 {
-    t_vec *res;
+	t_vector *res;
 
-	res = gc_malloc(rt->gc, sizeof(t_vec));
-    res->x = a->y * b->z - a->z * b->y;
-    res->y = a->z * b->x - a->x * b->z;
-    res-> z = a->x * b->y - a->y * b->x;
-    return (res);
+	res = gc_malloc(rt->gc, sizeof(t_vector));
+	res->x = a->y * b->z - a->z * b->y;
+	res->y = a->z * b->x - a->x * b->z;
+	res-> z = a->x * b->y - a->y * b->x;
+	return (res);
 }
 
-t_vec   *v_normalize(t_vec *v)
+t_vector	*v_normalize(t_vector *v)
 {
-    float   length;
+	float	length;
 
-    length = 0;
-    length = v_len(v);
-    if (!length)
-        return ((t_vec{0,0,0}));
-    v->x /= length;
-    v->y /= length;
-    v->z /= length;
-    return (v);
+	length = 0;
+	length = v_len(v);
+	if (!length)
+		return ((t_vector{0,0,0}));
+	v->x /= length;
+	v->y /= length;
+	v->z /= length;
+	return (v);
 }
 
-t_vec	*v_add(t_vec *a, t_vec *b)
+t_vector	*v_add(t_rt *rt, t_vector *a, t_vector *b)
 {
-	t_vec *res;
+	t_vector	*res;
 
-	res = gc_malloc(rt->gc, sizeof(t_vec));
+	res = gc_malloc(rt->gc, sizeof(t_vector));
 	res->x = a->x + b->x;
 	res->y = a->y + b->y;
 	res->z = a->z + b->z;
 	return (res);
-}
-
-t_point   *pv_add(t_vec *a, t_point *b)
-{
-    t_point *res;
-
-    res = gc_malloc(rt->gc, sizeof(t_point));
-    res->x = a->x + b->x;
-    res->y = a->y + b->y;
-    res->z = a->z + b->z;
-    return (res);
-}
-t_point	calc_endpoint_vector(t_vector *v, t_point *start, float scalar)
-{
-	t_point	 end;
-
-	end.x = start->x + scalar * v->x;
-	end.y = start->y + scalar * v->y;
-	end.z = start->z + scalar * v->z;
-	return(end);
 }
