3123fb1689703b82340b138413b53a2e4370258b
afoth <afoth@student.42berlin.de>
2025-02-04 16:17:41 +0100

    working on the plane_ray_intersection

--- src/plane_equations.c
+++ src/plane_equations.c
@@ -1,1 +1,47 @@
-#include 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   plane_equations.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afoth <afoth@student.42berlin.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 14:27:20 by afoth             #+#    #+#             */
+/*   Updated: 2025/02/04 14:42:03 by afoth            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/miniRT.h"
+
+/* DEL
+int	intersection_plane(double *t, t_plane *plane, t_ray *ray)
+{
+	t_vec3	v;
+	double	k;
+	double	b;
+
+	copy_vec3(&v, ray->terminus);
+	substract_vec3(&v, plane->point);
+	k = dot_product_vec3(ray->vec, plane->norm);
+	b = dot_product_vec3(&v, plane->norm);
+	if (k == 0)
+	{
+		*t = NO_ROOTS;
+		return (1);
+	}
+	*t = -b / k;
+	if (*t <= EPSILON_ROOT)
+		*t = NO_ROOTS;
+	return (*t == NO_ROOTS);
+}*/
+/* t = - ((lo -a) *n)/(l *n)
+a = plane point, n = nomalized vektor plane
+lo = startpoint ray, l vektor ray
+
+Ray equation = p = lo + tl
+P = lo + (- ((lo -a) *n)/(l *n)) *l
+ */
+
+t_point	*plane_ray_intersection(t_plane *pl, t_vector *v, t_point *p)
+{
+	
+}
