42341a22015c79acfa43e2ce07394ac866c5179f
afoth <afoth@student.42berlin.de>
2025-02-13 15:42:40 +0100

    merging, adde l/r/dplane ft for frsutum check

--- src/frustum_check.c
+++ src/frustum_check.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   frustum_check.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afoth <afoth@student.42berlin.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/13 15:39:19 by afoth             #+#    #+#             */
+/*   Updated: 2025/02/13 15:41:28 by afoth            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/miniRT.h"
 
 
@@ -24,4 +36,77 @@ void	frustum_check_uplane(t_rt *rt, int i)
 		rt->obj[i]->visible = 1;
 	else
 		rt->obj[i]->visible = 0;
+}
+
+void	frustum_check_dplane(t_rt *rt, int i)
+{
+	t_vector	*pos;
+	float		pos_prime_len;
+	float		dist_plane;
+
+	t_vector *pos_prime; // position projected onto n
+	if (rt->obj[i]->type == CYLINDER)
+		pos = v_between_two_points({0, 0, 0}, rt->obj[i]->cylinder->p);
+	else
+		pos = v_between_two_points({0, 0, 0}, rt->obj[i]->sphere->p);
+	pos_prime = vector_projection(pos, rt->fc->dplane_n);
+	pos_prime_len = v_len(pos_prime);
+	dist_plane = pos_prime_len + rt->fc->dplane_d;
+	if (rt->obj[i]->type == CYLINDER)
+		dist_plane += rt->obj[i]->cylinder->r;
+	else
+		dist_plane += rt->obj[i]->sphere->r;
+	if (dist_plane > 0 && rt->obj[i]->visible == 1)
+		rt->obj[i]->visible = 1;
+	else
+		rt->obj[i]->visible = 0;
+}
+
+void	frustum_check_rplane(t_rt *rt, int i)
+{
+	t_vector	*pos;
+	float		pos_prime_len;
+	float		dist_plane;
+
+	t_vector *pos_prime; // position projected onto n
+	if (rt->obj[i]->type == CYLINDER)
+		pos = v_between_two_points({0, 0, 0}, rt->obj[i]->cylinder->p);
+	else
+		pos = v_between_two_points({0, 0, 0}, rt->obj[i]->sphere->p);
+	pos_prime = vector_projection(pos, rt->fc->rplane_n);
+	pos_prime_len = v_len(pos_prime);
+	dist_plane = pos_prime_len + rt->fc->rplane_d;
+	if (rt->obj[i]->type == CYLINDER)
+		dist_plane += rt->obj[i]->cylinder->r;
+	else
+		dist_plane += rt->obj[i]->sphere->r;
+	if (dist_plane > 0 && rt->obj[i]->visible == 1)
+		rt->obj[i]->visible = 1;
+	else
+		rt->obj[i]->visible = 0;
+}
+
+void	frustum_check_lplane(t_rt *rt, int i)
+{
+	t_vector	*pos;
+	float		pos_prime_len;
+	float		dist_plane;
+
+	t_vector *pos_prime; // position projected onto n
+	if (rt->obj[i]->type == CYLINDER)
+		pos = v_between_two_points({0, 0, 0}, rt->obj[i]->cylinder->p);
+	else
+		pos = v_between_two_points({0, 0, 0}, rt->obj[i]->sphere->p);
+	pos_prime = vector_projection(pos, rt->fc->lplane_n);
+	pos_prime_len = v_len(pos_prime);
+	dist_plane = pos_prime_len + rt->fc->lplane_d;
+	if (rt->obj[i]->type == CYLINDER)
+		dist_plane += rt->obj[i]->cylinder->r;
+	else
+		dist_plane += rt->obj[i]->sphere->r;
+	if (dist_plane > 0 && rt->obj[i]->visible == 1)
+		rt->obj[i]->visible = 1;
+	else
+		rt->obj[i]->vis
+		ible = 0;
 }
